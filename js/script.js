6282b3620e2ae06509bfb0b793a2a6deaf4548f5
Henrique <mirandaferreirahenrique@hotmail.com>
2019-10-09 12:58:53 -0300

    scroll suave em classe

--- js/script.js
+++ js/script.js
@@ -1,23 +1,21 @@
-//Importando o default do arquivo
-//O nome do import não precisa ser igual ao nome da função neste caso
-import initScrollSuave from './modules/scroll-suave.js'
-import initAnimacaoScroll from './modules/scroll-animacao.js'
-import initAccordion from './modules/accordion.js'
-//Import individual deve informar o nome das funcoes
-//Ideal importar apenas um modulo
-import { initTabNav } from './modules/tab.js'
-import initModal from './modules/modal.js'
-import initTooltip from './modules/tooltip.js'
-import initDropdown from './modules/dropdown.js'
-import initMenuMobile from './modules/menu-mobile.js'
+import ScrollSuave from './modules/scroll-suave.js';
+import initAnimacaoScroll from './modules/scroll-animacao.js';
 
-import initFuncionamento from './modules/funcionamento.js'
-import initFetchAnimais from './modules/fetch-animais.js'
-import initFetchBtc from './modules/fetch-btc.js'
+import { initTabNav } from './modules/tab.js';
+import initModal from './modules/modal.js';
+import initTooltip from './modules/tooltip.js';
+import initDropdown from './modules/dropdown.js';
+import initMenuMobile from './modules/menu-mobile.js';
 
-initScrollSuave();
+import initFuncionamento from './modules/funcionamento.js';
+import initFetchAnimais from './modules/fetch-animais.js';
+import initFetchBtc from './modules/fetch-btc.js';
+
+const scrollSuave = new ScrollSuave('[data-menu=suave] a[href^="#"]');
+scrollSuave.init();
+
 initAnimacaoScroll();
-initAccordion();
+
 initTabNav();
 initModal();
 initTooltip();
@@ -27,5 +25,3 @@ initMenuMobile();
 initFuncionamento();
 initFetchAnimais();
 initFetchBtc();
-
-
