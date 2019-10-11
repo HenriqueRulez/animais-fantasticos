4210f02b0c58b4874a4a1617cf5d6b06feee40c4
Henrique <mirandaferreirahenrique@hotmail.com>
2019-10-11 09:37:25 -0300

    modal refatorada

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
 import ScrollSuave from './modules/scroll-suave.js';
-import initAnimacaoScroll from './modules/scroll-animacao.js';
 import TabNav from './modules/tab.js';
 import Accordion from './modules/accordion.js';
 import Modal from './modules/modal.js';
+import Tooltip from './modules/tooltip.js';
 
-import initTooltip from './modules/tooltip.js';
 import initDropdown from './modules/dropdown.js';
 import initMenuMobile from './modules/menu-mobile.js';
 import initFuncionamento from './modules/funcionamento.js';
 import initFetchAnimais from './modules/fetch-animais.js';
 import initFetchBtc from './modules/fetch-btc.js';
+import initAnimacaoScroll from './modules/scroll-animacao.js';
 
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
 scrollSuave.init();
@@ -30,7 +30,9 @@ const modal = new Modal(
 );
 modal.init();
 
-initTooltip();
+const toolTip = new Tooltip('[data-tooltip]');
+toolTip.init();
+
 initDropdown();
 initMenuMobile();
 initAnimacaoScroll();
